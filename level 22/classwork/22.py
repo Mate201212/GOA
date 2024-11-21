ea0f717ebda8836083afe04c28c2b6dc0ee8c3fa
Mate201212 <basharulimate50@gmail.com>
2024-11-21 16:09:26 +0400

    second commit

---
+++ level 22/classwork/22.py
@@ -0,0 +1,49 @@
+#1
+my_list = ["mate",0.356,890]
+print(my_list)
+
+
+#2
+my_list = ["shawarma","burger","pizza","sushi"]
+print(my_list)
+
+
+#3
+my_list = [20,40,60,80,100,120,140,160,180,200]
+print(my_list[4])
+
+#4
+my_list = [2,4,6,8,10,12,14,16,18,20,22,24,26,28,30]
+print(my_list[:10])
+
+
+
+
+
+
+
+#1
+favorite_cars = [
+    "Porsche 911",
+    "Tesla ",
+    "Lamborghini ",
+    "Audi ",
+    "Ferrari ",
+    "Jeep ",
+    "Mercedes",
+    "mclaren"]
+
+
+print("favourite cars")
+
+#2 
+favorite_cars = [
+    "Porsche 911",
+    "Tesla ",
+    "Lamborghini ",
+    "Audi ",
+    "Ferrari ",
+    "Jeep ",
+    "Mercedes",
+    "mclaren"]
+print("favourite cars"(6))
