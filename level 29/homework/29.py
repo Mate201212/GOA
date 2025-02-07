edbf830c71f670a3cea46ea4f48bfdffc888427f
Mate201212 <basharulimate50@gmail.com>
2025-02-07 17:25:25 +0400

    second commit

---
+++ level 29/homework/29.py
@@ -0,0 +1,15 @@
+students = [
+    {"name": "ნიკა", "surname": "ბერიძე", "subject": "მათემატიკა", "score": 85},
+    {"name": "მარიამ", "surname": "შარიქაძე", "subject": "ფიზიკა", "score": 90},
+    {"name": "გიორგი", "surname": "ჯორჯაძე", "subject": "ქიმია", "score": 88},
+]
+trips = [
+    {"time": "2025-02-20", "destination": "პარიზი", "price": 350},
+    {"time": "2025-03-10", "destination": "რომი", "price": 450},
+    {"time": "2025-05-15", "destination": "ათენი", "price": 300},
+]
+tournament = [
+    {"team": "მილანი", "points": 18, "wins": 6},
+    {"team": "ბარსელონა", "points": 21, "wins": 7},
+    {"team": "რეალი", "points": 15, "wins": 5},
+]
