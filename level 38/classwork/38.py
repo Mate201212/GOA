cd9452c4320aae6fb36ce3f2ef8297c6bc5cbb03
Mate201212 <basharulimate50@gmail.com>
2025-05-06 17:40:48 +0400

    commit

---
+++ level 38/classwork/38.py
@@ -0,0 +1,38 @@
+def add_numbers(g,v):
+    return a + b 
+
+result = add_numbers(7, 2)
+print(result)
+
+
+
+
+
+def square_number(t):
+    return t * t
+
+
+result = square_number(1)
+print(result) 
+
+
+
+
+
+
+def greet_user(name):
+    print(f"hello, {name}!")
+    
+user_name = input("enter your name: ")
+greet_user(user_name)
+
+
+
+
+
+
+def greet_multiple_times(name, times):
+    for _ in range(times):
+        print(f"hello!, {name}!")
+
+
